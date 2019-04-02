f9db4c13f22596fbb4add4c5da0d60e1347dae24
henry.bk <henrique.kubenda@gmail.com>
2019-04-02 12:31:45 -0300

    algunos ajustes a los comentarios y algunas pruebas a las funcionalidades de routing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for   #importando el objeto Flask y la funcion render_template
+from flask import Flask, render_template, url_for   #importando el objeto Flask , la funcion render_template y url_for
 
 app = Flask(__name__) # creando el webserver, __name__ hace referencia al archivo actual (__name__ = main.py)
 
@@ -10,7 +10,11 @@ def home ():
 def about ():
     return render_template("about.html")
 
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
 if __name__ == "__main__": #cuando se ejectuta el script python le asigna el nombre "__main__" al script 
-    app.run(debug = True)  #por si se importa otro scipt, el if previene que otros scripts se ejecuten,
+    app.run(debug = True)  #por si se importa otro script, el if previene que otros scripts se ejecuten,
                            #cuando se ejecute el archivo main.py solo entonces su nombre sera cambiado a __main__
-                           #y solo entonces se ejecutara el if
+                           #y solo entonces se ejecutara el if (linea 15) 
